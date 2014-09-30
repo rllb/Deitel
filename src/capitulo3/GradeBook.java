ba3c04d5f77e4b0b47cd04a560825329c02ffe57
PedroSous <contato.pedrosousa@gmail.com>
2014-09-29 21:28:08 -0300

    Iniciando o capitulo 3

---
+++ src/capitulo3/GradeBook.java
@@ -0,0 +1,10 @@
+package capitulo3;
+
+public class GradeBook {
+	
+	//exibe uma mensagem  de boas-vindas para o usuário  GrandeBook
+	public void displayMessage(){
+		System.out.println("Welcometo the Grade Book!");
+	}// fim do método displayMessage
+
+}// fim da classe GradeBook
---
+++ src/capitulo3/GradeBookTest.java
@@ -0,0 +1,15 @@
+package capitulo3;
+
+public class GradeBookTest {
+
+	// método main inicia a execução de programa
+	public static void main(String [] args){
+		//cria um objeto GradeBook e o atribui a myGradeBook
+		GradeBook myGradeBook =new GradeBook();
+		
+		//chama método displayMessage de myGradeBook
+		myGradeBook.displayMessage();
+		
+	}//fim de main
+	
+}// fim da classe GradeBookTest
