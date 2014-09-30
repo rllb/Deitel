9625346c631aa5c3604751c68766d98b02015335
PedroSousa <contato.pedrosousa@gmail.com>
2014-09-30 07:52:30 -0300

    Acrescentando Get and Set na Gradebook

--- src/capitulo3/GradeBook.java
+++ src/capitulo3/GradeBook.java
@@ -2,10 +2,22 @@ package capitulo3;
 
 public class GradeBook {
 	
+	private String courseName;// nome do curso para esse GradeBook
+	
+	//método para configurar o nome do curso
+	public void setCourseName(String name){
+		courseName = name;// armazena o nome do curso
+	}// fim do método setCourseName
+	
+	//método para recuperar o nome do curso
+	public String getCourseName(){
+		return courseName;
+	}//fim do mérotodo getCourseName
+	
 	//exibe uma mensagem  de boas-vindas para o usuÃ¡rio  GrandeBook 
-	// Seja bem vindo
+	// o curso que essa Gradebook repesenta
 	public void displayMessage(String courseName){
-		System.out.printf("Welcometo the Grade Book for \n%s!\n", courseName);
+		System.out.printf("Welcometo the Grade Book for \n%s!\n", getCourseName());
 	}// fim do mÃ©todo displayMessage
 
 }// fim da classe GradeBook
